e1fb269a0e2d5cdd3d3ba71c909d4fc00639c58c
OneDollarCult <OneDollarCult@proton.me>
2025-08-06 14:40:33 +0300

    Delete fetchPlayers.js

--- fetchPlayers.js
+++
@@ -1,6 +0,0 @@
-fetch('https://YOUR_USERNAME.github.io/players.json')
-  .then(res => res.json())
-  .then(data => {
-    const count = data.players.length;
-    document.getElementById('playersCount').innerText = count;
-  });
